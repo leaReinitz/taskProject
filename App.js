6f3c54355dcb7a3404d7232705c903bf054feb0f
lea reinitz <lea@leader.codes>
2022-08-30 23:03:56 +0300

    build change5

--- App.js
+++ App.js
@@ -28,9 +28,9 @@ app.use(express.static(
 
 app.use('/',routerApi)
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
 
 app.listen(process.env.PORT, () => {
     console.log('listening !!!!!!!');
